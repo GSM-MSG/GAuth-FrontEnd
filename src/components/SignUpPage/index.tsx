c35240852257ed789ea425df466a723626e25cd4
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2022-10-06 14:02:24 +0900

    회원가입페이지 퍼블리싱

---
+++ src/components/SignUpPage/index.tsx
@@ -0,0 +1,140 @@
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
+import * as S from './style';
+import * as SVG from '../../SVG';
+
+export default function SignUpPage() {
+	const waveRef = useRef<any>();
+	const signUpRef = useRef<any>();
+	const [email, setEmail] = useState<string>('');
+	const [pw, setPw] = useState<string>('');
+	const [emailCheck, setEmailCheck] = useState<boolean>(false);
+	const [pwCheck, setPwCheck] = useState<boolean>(false);
+
+	useEffect(() => {
+		waveRef.current.children[0].style.left =
+			-signUpRef.current.offsetWidth + 'px';
+		waveRef.current.children[1].style.left =
+			-signUpRef.current.offsetWidth + 'px';
+		window.addEventListener('resize', () => {
+			const width = signUpRef.current.offsetWidth;
+			waveRef.current.children[0].style.left = -width + 'px';
+			waveRef.current.children[1].style.left = -width + 'px';
+		});
+	}, []);
+
+	return (
+		<>
+			<S.Layer>
+				<S.WaveWrapper ref={waveRef}>
+					<S.SideWave
+						fill="url(#gradient1)"
+						options={{
+							height: 10,
+							amplitude: 150,
+							speed: 0.185,
+							points: 2,
+						}}
+					>
+						<defs>
+							<linearGradient id="gradient1" gradientTransform="rotate(180)">
+								<stop offset="0%" stopColor="#7EB2E2" />
+								<stop offset="100%" stopColor="#5499D9" />
+							</linearGradient>
+						</defs>
+					</S.SideWave>
+					<S.SideWave
+						fill="url(#gradient2)"
+						options={{
+							height: 30,
+							amplitude: 150,
+							speed: 0.17,
+							points: 2,
+						}}
+					>
+						<defs>
+							<linearGradient id="gradient2" gradientTransform="rotate(180)">
+								<stop offset="0%" stopColor="#5499D9" />
+								<stop offset="100%" stopColor="#2E80CC" />
+							</linearGradient>
+						</defs>
+					</S.SideWave>
+					<S.Title>
+						<div>
+							<S.WaterDrop>
+								<span></span>
+								<span></span>
+								<span></span>
+							</S.WaterDrop>
+							<SVG.Human1 />
+						</div>
+						<div>
+							<S.WaterDrop>
+								<span></span>
+								<span></span>
+								<span></span>
+							</S.WaterDrop>
+							<SVG.SmallPhone />
+						</div>
+						<div>
+							<S.WaterDrop>
+								<span></span>
+								<span></span>
+								<span></span>
+							</S.WaterDrop>
+							<SVG.Human2 />
+						</div>
+					</S.Title>
+				</S.WaveWrapper>
+				<S.LoginWrapper ref={signUpRef}>
+					<S.LoginContainer>
+						<h1>SIGNUP</h1>
+						<S.InputContainer>
+							<S.InputWrapper>
+								<S.InputName being={emailCheck}>이메일</S.InputName>
+								<input
+									name="email"
+									type="text"
+									maxLength={6}
+									value={email}
+									onChange={(e: any) => setEmail(e.target.value)}
+									onFocus={() => {
+										setEmailCheck(true);
+									}}
+									onBlur={() => {
+										!email && setEmailCheck(false);
+									}}
+								/>
+								{email && (
+									<S.Email left={email.length * 13.5}>@gsm.hs.kr</S.Email>
+								)}
+							</S.InputWrapper>
+							<S.InputWrapper>
+								<S.InputName being={pwCheck}>비밀번호</S.InputName>
+								<input
+									name="pw"
+									type="password"
+									maxLength={20}
+									onChange={(e: any) => setPw(e.target.value)}
+									onFocus={() => {
+										setPwCheck(true);
+									}}
+									onBlur={() => {
+										!pw && setPwCheck(false);
+									}}
+								/>
+							</S.InputWrapper>
+						</S.InputContainer>
+						<S.ButtonContainer>
+							<S.Submit>회원가입</S.Submit>
+							<div>
+								<Link href="/register">회원가입</Link> |{' '}
+								<Link href="/find">비밀번호 찾기</Link>
+							</div>
+						</S.ButtonContainer>
+					</S.LoginContainer>
+				</S.LoginWrapper>
+			</S.Layer>
+		</>
+	);
+}
---
+++ src/components/SignUpPage/style.ts
@@ -0,0 +1,217 @@
+import styled from '@emotion/styled';
+import Wave from 'react-wavify';
+
+export const Layer = styled.div`
+	width: 100vw;
+	height: 100vh;
+	top: 0;
+	left: 0;
+	display: flex;
+
+	> div {
+		/* width: 100%; */
+		height: 100vh;
+	}
+`;
+
+export const WaveWrapper = styled.div`
+	width: 200vw;
+	background: #d1e4f5;
+`;
+
+export const SideWave = styled(Wave)`
+	position: fixed;
+	bottom: 0;
+	transform-origin: bottom;
+	height: 950px;
+`;
+
+export const Title = styled.div`
+	position: relative;
+	div {
+		position: absolute;
+		display: flex;
+		:nth-child(1) {
+			top: 220px;
+			animation: humanAnimation 4s linear infinite;
+		}
+		:nth-child(2) {
+			top: 250px;
+			left: 450px;
+			animation: humanAnimation 4s linear infinite;
+			animation-delay: 1s;
+		}
+		:nth-child(3) {
+			right: 0;
+			top: 500px;
+			animation: humanAnimation 4s linear infinite;
+			animation-delay: 2s;
+		}
+	}
+	@keyframes humanAnimation {
+		0%,
+		100% {
+			transform: translateY(0);
+		}
+		50% {
+			transform: translateY(20px);
+		}
+	}
+	@keyframes Air {
+		0%,
+		100% {
+			opacity: 0;
+			transform: translateY(0);
+		}
+		15% {
+			transform: translateY(-15px) translateX(5px);
+		}
+		35% {
+			width: 7.5px;
+			height: 7.5px;
+			transform: translateY(-35px) translateX(-5px);
+		}
+		60% {
+			opacity: 1;
+			transform: translateY(-55px) translateX(10px);
+			width: 15px;
+			height: 15px;
+		}
+		60.1% {
+			opacity: 0;
+		}
+	}
+`;
+
+export const WaterDrop = styled.div`
+	position: absolute;
+	span {
+		position: absolute;
+		width: 1px;
+		height: 1px;
+		border-radius: 50%;
+		opacity: 0;
+		background: white;
+		animation: Air 5s linear infinite;
+		:nth-child(1) {
+			animation-delay: 2s;
+		}
+		:nth-child(2) {
+			animation-delay: 3s;
+		}
+		:nth-child(3) {
+			animation-delay: 4s;
+		}
+	}
+`;
+
+export const LoginWrapper = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 10;
+`;
+
+export const LoginContainer = styled.div`
+	width: 425px;
+	display: flex;
+	flex-direction: column;
+	gap: 80px;
+	justify-content: center;
+
+	h1 {
+		text-align: center;
+		font-size: 60px;
+		font-weight: 100;
+		letter-spacing: 5px;
+		margin: 0;
+		color: #2e80cc;
+	}
+`;
+
+export const InputContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 40px;
+	input {
+		border: none;
+		border-bottom: 3px solid black;
+		width: 100%;
+		padding: 0 30px 10px 30px;
+		font-size: 27px;
+		font-weight: 400;
+		outline: none;
+		background-color: transparent;
+	}
+
+	h2 {
+		position: relative;
+		margin: 0px auto -60px auto;
+		font-size: 27px;
+		font-weight: 400;
+	}
+`;
+
+export const InputWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	gap: 10px;
+	position: relative;
+`;
+
+export const Email = styled.div`
+	position: absolute;
+	top: -5px;
+	left: ${({ left }: { left: number }) => {
+		return left + 40 + 'px';
+	}};
+	font-size: 27px;
+`;
+
+export const InputName = styled.h3`
+	color: #929292;
+	position: absolute;
+	margin: 0;
+	transition: 0.5s;
+	${({ being }: { being: boolean }) => {
+		if (being) {
+			return 'transform: translateY(-25px); font-size: 15px;';
+		}
+	}};
+`;
+
+export const ButtonContainer = styled.div`
+	width: 80%;
+	margin: 0 auto;
+	text-align: center;
+	font-size: 20px;
+	font-size: 18px;
+	line-height: 21px;
+
+	div {
+		color: #929292;
+	}
+`;
+
+export const Submit = styled.button`
+	width: 100%;
+	height: 60px;
+	color: #fff;
+	background: #2e80cc;
+	font-weight: 590;
+	font-size: 27px;
+	line-height: 32px;
+	text-align: center;
+	border: none;
+	border-radius: 30px;
+	margin-bottom: 10px;
+	cursor: pointer;
+
+	&:active {
+		box-shadow: rgba(50, 50, 93, 0.15) 0px 30px 60px -12px inset,
+			rgba(0, 0, 0, 0.2) 0px 18px 36px -18px inset;
+	}
+`;

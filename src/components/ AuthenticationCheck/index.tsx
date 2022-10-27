6ede76c0b61f2d83ed57e8a262726a3976393ceb
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2022-10-28 07:52:24 +0900

    Add:회원가입 페이지 인증 확인 모달 추가

--- src/components/ AuthenticationCheck/index.tsx
+++ src/components/ AuthenticationCheck/index.tsx
@@ -5,12 +5,18 @@ export default function AuthenticationCheck() {
 	return (
 		<S.Layer>
 			<S.Wrapper>
-				<h2>인증 메일이 전송되었습니다!</h2>
+				{/* <h2>인증 메일이 전송되었습니다!</h2>
 				<p>
 					전송된 메일에서 <span>인증 버튼</span>을 눌러 인증을 완료해주세요
 				</p>
 				<SVG.Mail></SVG.Mail>
-				<button type="button">확인</button>
+				<button type="button">확인</button> */}
+				<h2>
+					인증이 완려되었습니다!
+					<br />
+					<br />
+					과리자 승인을 기다려 주세요
+				</h2>
 			</S.Wrapper>
 		</S.Layer>
 	);

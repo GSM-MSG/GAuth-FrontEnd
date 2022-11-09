4396929267979ce72a06e63b2f571a417aaa9fd9
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2022-11-09 20:41:02 +0900

    Update:public svg로 svg componenet 변경

--- src/components/SignUpPage/index.tsx
+++ src/components/SignUpPage/index.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
 import * as S from './style';
-import * as SVG from '../../SVG';
-import AuthenticationCheck from '../ AuthenticationCheck';
 
 export default function SignUpPage() {
 	const waveRef = useRef<any>();
@@ -86,7 +84,7 @@ export default function SignUpPage() {
 								<span></span>
 								<span></span>
 							</S.WaterDrop>
-							<SVG.Human1 />
+							<img src="/svg/Human1.svg" />
 						</div>
 						<div>
 							<S.WaterDrop top="50px" left="-20px" rotate="-50deg">
@@ -94,7 +92,8 @@ export default function SignUpPage() {
 								<span></span>
 								<span></span>
 							</S.WaterDrop>
-							<SVG.SmallPhone />
+
+							<img src="/svg/SmallPhone.svg" />
 						</div>
 						<div>
 							<S.WaterDrop top="120px" left="250px" rotate="-50deg">
@@ -102,7 +101,7 @@ export default function SignUpPage() {
 								<span></span>
 								<span></span>
 							</S.WaterDrop>
-							<SVG.Human2 />
+							<img src="/svg/Human2.svg" />
 						</div>
 					</S.Title>
 				</S.WaveWrapper>
@@ -160,7 +159,7 @@ export default function SignUpPage() {
 					</S.SignUpContainer>
 				</S.SignUpWrapper>
 			</S.Layer>
-			<AuthenticationCheck />
+			{/* <AuthenticationCheck /> */}
 		</>
 	);
 }

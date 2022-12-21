f6c702c42a1c6edce794e6a72b6876b33e02c3b7
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2022-12-22 07:48:30 +0900

    Create:MyProfile page publishing and img add 기능 create

---
+++ src/components/MyProfilePage/style.ts
@@ -0,0 +1,109 @@
+import styled from '@emotion/styled';
+
+export const Positioner = styled.div`
+	width: 100%;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+export const Layer = styled.div`
+	width: 65vw;
+	height: 100%;
+	@media (max-width: 1300px) {
+		width: 400px;
+	}
+`;
+
+export const TitleSection = styled.div`
+	margin-top: 150px;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	h1 {
+		font-family: 'Pretendard';
+	}
+	h3 {
+		color: #929292;
+	}
+`;
+
+export const ProfileSection = styled.div`
+	width: 100%;
+	height: 178px;
+	display: flex;
+	margin-top: 80px;
+`;
+
+export const UpLoadProfileContainter = styled.div`
+	display: flex;
+`;
+
+export const ProfileSVGWrapper = styled.div`
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	label {
+		cursor: pointer;
+		i {
+			position: absolute;
+			top: 130px;
+			left: 130px;
+		}
+		:hover {
+			i {
+				svg {
+					path {
+						fill: #868e96;
+					}
+				}
+			}
+		}
+	}
+`;
+
+export const Profile = styled.img`
+	margin: 0px;
+	padding: 2px;
+	width: 178px;
+	height: 178px;
+	border-radius: 50%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	object-fit: cover;
+	background: #fff;
+`;
+
+export const PrivacySection = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	margin-left: 47px;
+	div {
+		display: flex;
+		align-items: center;
+		gap: 10px;
+		h1 {
+			font-size: 45px;
+			font-weight: 700;
+			color: #000000;
+		}
+		p {
+			font-size: 27px;
+			font-weight: 600;
+			color: #000000;
+		}
+	}
+	h3 {
+		font-size: 27px;
+		font-weight: 300;
+		color: #929292;
+	}
+`;
+
+export const Section = styled.div``;
--- src/lib/API.tsx
+++ src/lib/API.tsx
@@ -1,40 +1,41 @@
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export const API = axios.create({
-    baseURL: "http://10.120.74.223:8080",
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-        "Access-Control-Allow-Headers": "x-access-token, Origin, X-Requested-With, Content-Type, Accept",
-        "Access-Control-Alllow-Credentials": true,
-    },
+	baseURL: 'http://10.120.74.223:8080',
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+		'Access-Control-Allow-Headers':
+			'x-access-token, Origin, X-Requested-With, Content-Type, Accept',
+		'Access-Control-Alllow-Credentials': true,
+	},
 });
 
 API.interceptors.request.use(
-    async (config: AxiosRequestConfig) => {
-        if (config.headers) {
-            try {
-                const { data } = await API.patch('/auth', {
-                    headers: { 'RefreshToken': localStorage.getItem('Gauth-refreshToken') }
-                })
+	async (config: AxiosRequestConfig) => {
+		if (config.headers) {
+			try {
+				const { data } = await API.patch('/auth', {
+					headers: { RefreshToken: localStorage.getItem('Gauth-refreshToken') },
+				});
 
-                config.headers['Authorization'] = await "Bearer " + data.accessToken;
-                localStorage.setItem('Gauth-accessToken', data.accessToken);
-                localStorage.setItem('Gauth-refreshToken', data.refreshToken);
-
-            } catch (error: any) {
-                if (error.message === 'Request failed with status code 401' ||
-                    error.message === 'Request failed with status code 404') {
-
-                    localStorage.removeItem('Gauth-accessToken');
-                    localStorage.removeItem('Gauth-refreshToken');
-                    window.location.replace('/login');
-                }
-            }
-        }
-        return config;
-    },
-    async (error: AxiosError) => {
-        return Promise.reject(error);
-    }
+				config.headers['Authorization'] = (await 'Bearer ') + data.accessToken;
+				localStorage.setItem('Gauth-accessToken', data.accessToken);
+				localStorage.setItem('Gauth-refreshToken', data.refreshToken);
+			} catch (error: any) {
+				if (
+					error.message === 'Request failed with status code 401' ||
+					error.message === 'Request failed with status code 404'
+				) {
+					localStorage.removeItem('Gauth-accessToken');
+					localStorage.removeItem('Gauth-refreshToken');
+					window.location.replace('/login');
+				}
+			}
+		}
+		return config;
+	},
+	async (error: AxiosError) => {
+		return Promise.reject(error);
+	}
 );

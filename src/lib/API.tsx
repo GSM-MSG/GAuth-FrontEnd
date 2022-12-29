329b5605d61b18459e1936fbcab7f34303d41b4a
kangkong <kang3530554@gmail.com>
2022-12-29 20:26:39 +0900

    Feat: token 유효기한에 따른 관리 로직 추가

--- src/lib/API.tsx
+++ src/lib/API.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import Router from 'next/router';
-import { accessToken, refreshToken } from './Token';
+import { accessToken, expiredAt, refreshToken } from './Token';
 
 export const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -18,11 +18,14 @@ API.interceptors.request.use(
   (config: AxiosRequestConfig) => {
     const access_token: string | null = localStorage.getItem(accessToken);
     const refresh_token: string | null = localStorage.getItem(refreshToken);
+    const token_expiredAt: string = localStorage.getItem(expiredAt) ?? '';
 
     if (config.headers) {
-      config.headers['Authorization'] = access_token
-        ? `Bearer ${access_token}`
-        : '';
+      config.headers['Authorization'] =
+        access_token &&
+        new Date(token_expiredAt).getTime() - new Date().getTime() > 30000
+          ? `Bearer ${access_token}`
+          : '';
 
       config.headers['refreshToken'] = refresh_token
         ? `Bearer ${refresh_token}`
@@ -42,11 +45,7 @@ API.interceptors.response.use(
   },
 
   async (err: AxiosError) => {
-    if (
-      err.config.headers &&
-      err.response &&
-      (err.response.status === 401 || err.response.status === 401)
-    ) {
+    if (err.config.headers && err.response && err.response.status === 401) {
       const refresh_token: string | null =
         localStorage.getItem(refreshToken) ?? '';
       try {
@@ -60,13 +59,14 @@ API.interceptors.response.use(
           }
         );
 
-        axios.defaults.headers.common['Authorization'] = `${data.accessToken}`;
         localStorage.setItem(accessToken, data.accessToken);
         localStorage.setItem(refreshToken, data.refreshToken);
+        localStorage.setItem(expiredAt, data.expiresAt);
         return await API.request(err.config);
       } catch (err) {
         localStorage.removeItem(accessToken);
         localStorage.removeItem(refreshToken);
+        localStorage.removeItem(expiredAt);
         Router.replace('/login');
         return Promise.reject(err);
       }

4cedaadd9f70bdec631d98461d9a85cf165a5b02
kangkong <kang3530554@gmail.com>
2023-03-06 23:11:37 +0900

    Fix password reg exp

--- src/components/SignUpPage/index.tsx
+++ src/components/SignUpPage/index.tsx
@@ -154,7 +154,8 @@ export default function SignUpPage() {
                 <S.Submit
                   onClick={() => {
                     const reg_pw =
-                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,72}/;
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&^#])[A-Za-z\d$@$!%*?&^#]{8,72}/;
+
                     if (/^.{0}$/.test(watch('email'))) {
                       toast.error('이메일 입력을 확인해주세요.');
                       setFocus('email');

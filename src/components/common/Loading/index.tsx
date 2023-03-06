980c3aee0a6463448241b6e2d53a9e06e4b1efde
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-03-07 01:38:58 +0900

    Update src/components/common/Loading/index.tsx
    Co-authored-by: 변찬우 <57276315+chanwoo00106@users.noreply.github.com>

--- src/components/common/Loading/index.tsx
+++ src/components/common/Loading/index.tsx
@@ -6,9 +6,9 @@ export function LightCube({ color }: { color: string }) {
     <S.cube>
       <S.top></S.top>
       <div>
-        {list.map((e) => {
-          return <S.span num={e} key={e} color={color}></S.span>;
-        })}
+        {list.map((e) => (
+          <S.span num={e} key={e} color={color} />
+        ))}
       </div>
       <S.bottom></S.bottom>
     </S.cube>

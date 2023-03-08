696518868102849b20aea70dbda2169ae26d8ad7
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-03-08 23:04:15 +0900

    Update main page new style

--- src/components/ServiceList/index.tsx
+++ src/components/ServiceList/index.tsx
@@ -3,14 +3,14 @@ import * as S from './style';
 
 export default function ServiceList() {
   return (
-    <S.Positioner>
-      <S.Layer>
+    <S.Layout>
+      <S.Wrapper>
         <S.TitleSection>
           <h1>GAUTH를 이용하는 서비스에요!</h1>
           <h3>이런 서비스들도 GAUTH를 이용해요!</h3>
         </S.TitleSection>
         <ListTable />
-      </S.Layer>
-    </S.Positioner>
+      </S.Wrapper>
+    </S.Layout>
   );
 }

e42b7c56ffa88d1efe578fc240aa9fe761137505
kangkong <kang3530554@gmail.com>
2023-03-10 10:22:16 +0900

    Add toast message in usefetch hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,40 +1,59 @@
-import { Method, isAxiosError, AxiosError } from 'axios';
-import { config } from 'process';
-import { useState, useEffect, useCallback } from 'react';
+import { Method, isAxiosError } from 'axios';
+import { useState, useCallback } from 'react';
+import { toast } from 'react-toastify';
 import API from '../api';
 
 interface Props<T> {
   url: string;
   method: Method;
-  body: any;
-  onSuccess: () => void | Promise<void>;
-  onFailure: () => void | Promise<void>;
+  onSuccess?: (data: T) => void | Promise<void>;
+  onFailure?: (e: unknown) => void | Promise<void>;
+  successMessage?: string;
+  errorMessage?: string;
 }
 
-const useFetch = <T>({ url, method, body, onSuccess, onFailure }: Props<T>) => {
+const useFetch = <T>({
+  url,
+  method,
+  onSuccess,
+  onFailure,
+  successMessage,
+  errorMessage,
+}: Props<T>) => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
-  const fetch = useCallback(async () => {
-    setLoading(true);
-    try {
-      const { data } = await API({
-        url: url,
-        method: method,
-        data: body,
-      });
-      setData(data);
-      onSuccess();
-    } catch (error) {
-      if (!isAxiosError(error)) {
-        onFailure();
+  const fetch = useCallback(
+    async (body: any) => {
+      setLoading(true);
+
+      try {
+        const { data } = await API({
+          url: url,
+          method: method,
+          data: body,
+        });
+        setData(data);
+
+        if (successMessage) toast.success(successMessage);
+        if (onSuccess) await onSuccess(data);
+      } catch (e) {
+        if (!isAxiosError(e))
+          return toast.error('알 수 없는 에러가 발생했습니다');
+        else if (e.response && e.response.status >= 500)
+          toast.error('알 수 없는 에러가 발생했습니다');
+        else if (errorMessage)
+          await toast.error('알 수 없는 에러가 발생했습니다');
+
+        if (onFailure) await onFailure(e);
+      } finally {
+        setLoading(false);
       }
-    } finally {
-      setLoading(false);
-    }
-  }, [url, method, body]);
+    },
+    [url, method, onSuccess, onFailure, successMessage, errorMessage]
+  );
 
-  return { data, loading, fetch };
+  return { data, isLoading, fetch };
 };
 
 export default useFetch;

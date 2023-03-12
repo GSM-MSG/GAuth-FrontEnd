7c1913b098846985e1ebb1d6b6a278621e3715db
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-03-13 01:37:40 +0900

    Update notion page style

--- src/components/InstructionPage/style.ts
+++ src/components/InstructionPage/style.ts
@@ -1,17 +1,30 @@
 import styled from '@emotion/styled';
 
-export const NotionPage = styled.div`
-  position: relative;
-  margin-left: 20%;
-  width: 80%;
-  right: 0;
-  top: 0;
+export const Layout = styled.div`
+  width: 100%;
+  min-height: 100vh;
   display: flex;
   align-items: center;
-  padding: 50px;
+  justify-content: center;
   header {
     max-height: 0;
     min-height: 0;
     opacity: 0;
+  }
+
+  @media (min-width: 801px) {
+    padding-left: 100px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  width: 1200px;
+  font-size: 20px;
+  @media (max-width: 1400px) {
+    width: 100%;
+    margin: 0 50px 50px;
+    > * {
+      font-size: 1.4vw;
+    }
   }
 `;

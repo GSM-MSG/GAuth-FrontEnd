5e70c9c7118ee2828cb5d1cbaf46fce10077f770
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-03-15 08:21:22 +0900

    Add hook finaly logic

--- src/types/hooks/useFetch.ts
+++ src/types/hooks/useFetch.ts
@@ -1,10 +1,11 @@
-import { Method } from 'axios';
+import { AxiosError, Method } from 'axios';
 
 interface Props<T> {
   url: string;
   method: Method;
   onSuccess?: (data: T) => void | Promise<void>;
-  onFailure?: (e: unknown) => void | Promise<void>;
+  onFailure?: (e: AxiosError) => void | Promise<void>;
+  onFinaly?: () => void;
   successMessage?: string;
   errorMessage?: { [status: number]: string };
 }

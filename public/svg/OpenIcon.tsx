ca76ca70afd1f9f4f7b1dd9d6c31a61e69d7d118
송현우 <101445027+googoo81@users.noreply.github.com>
2023-06-28 20:14:25 +0900

    Update OpenIcon.tsx

--- public/svg/OpenIcon.tsx
+++ public/svg/OpenIcon.tsx
@@ -8,8 +8,8 @@ export function OpenIcon() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M28 14C28 14 24 4.99994 14 5C4 5.00006 0 14 0 14C0 14 4 23 14 23C24 23 28 14 28 14ZM19 14C19 16.7614 16.7614 19 14 19C11.2386 19 9 16.7614 9 14C9 11.2386 11.2386 9 14 9C16.7614 9 19 11.2386 19 14ZM21 14C21 17.866 17.866 21 14 21C10.134 21 7 17.866 7 14C7 10.134 10.134 7 14 7C17.866 7 21 10.134 21 14Z"
         fill="#929292"
       />

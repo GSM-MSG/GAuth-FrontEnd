b93bdabef4a0e96040e07526ad1d0da8a3b5d20b
SeoJumee <official.jumee@gmail.com>
2023-09-11 09:00:12 +0900

    :lipstick: Update service modify modal

--- src/components/MyServiceList/Modify/index.tsx
+++ src/components/MyServiceList/Modify/index.tsx
@@ -78,90 +78,101 @@ export default function ModifyMyService() {
 
   return (
     <S.Wrapper>
-      <h1>서비스 정보 수정</h1>
-      <form onSubmit={handleSubmit(ModifyService, CheckError)}>
-        <div>
-          <Input
-            label={'서비스명'}
-            errors={!!errors.error}
-            register={register('serviceName', {
-              required: '서비스명을 입력하지 않았습니다.',
-              pattern: {
-                value: /\S+/,
-                message: '서비스명을 입력하지 않았습니다.',
-              },
-              maxLength: 29,
-            })}
-          />
-          <Input
-            label={'서비스 URI'}
-            errors={!!errors.error}
-            register={register('serviceUri', {
-              required: '사이트 URI를 입력하지 않았습니다.',
-              pattern: {
-                value: regUri,
-                message: '사이트 URI를 형식에 맞게 입력해주세요',
-              },
-              maxLength: 254,
-            })}
-          />
-          <Input
-            label={'리다이렉트 URI'}
-            errors={!!errors.error}
-            register={register('redirectUri', {
-              required: '사이트 URI를 입력하지 않았습니다.',
-              pattern: {
-                value: regUri,
-                message: '리다이렉트 URI를 형식에 맞게 입력해주세요',
-              },
-              maxLength: 254,
-            })}
-          />
-          <S.CopyWrapper>
-            <S.CopyTitle>
-              <p>
-                클라이언트 ID
-                <i
-                  onClick={() => {
-                    navigator.clipboard.writeText(watch('clientId'));
-                    toast.info('텍스트가 복사 되었습니다.');
-                  }}
-                >
-                  <SVG.CopyIcon />
-                </i>
-              </p>
-              <h4>{watch('clientId')}</h4>
-            </S.CopyTitle>
-            <S.CopyTitle>
-              <p>
-                클라이언트 SECRET
-                <i
-                  onClick={() => {
-                    navigator.clipboard.writeText(watch('clientSecret'));
-                    toast.info('텍스트가 복사 되었습니다.');
-                  }}
-                >
-                  <SVG.CopyIcon />
-                </i>
-              </p>
-              <h4>{watch('clientSecret')}</h4>
-            </S.CopyTitle>
-            <S.CopyTitle>
-              <S.Scope>
-                <p>공개 여부 :</p>
-                <div onClick={ChangeSeviceScope}>
-                  {serviceScope === 'PUBLIC' ? (
-                    <SVG.ServicePublic />
-                  ) : (
-                    <SVG.ServicePrivate />
-                  )}
-                </div>
-              </S.Scope>
-            </S.CopyTitle>
-          </S.CopyWrapper>
-        </div>
-        <button type="submit">수정</button>
-      </form>
+      <S.Section>
+        <h1>서비스 정보 수정</h1>
+        <form onSubmit={handleSubmit(ModifyService, CheckError)}>
+          <S.ContentSection>
+            <div>
+              <Input
+                label={'서비스명'}
+                errors={!!errors.error}
+                register={register('serviceName', {
+                  required: '서비스명을 입력하지 않았습니다.',
+                  pattern: {
+                    value: /\S+/,
+                    message: '서비스명을 입력하지 않았습니다.',
+                  },
+                  maxLength: 29,
+                })}
+              />
+              <Input
+                label={'리다이렉트 URI'}
+                errors={!!errors.error}
+                register={register('redirectUri', {
+                  required: '사이트 URI를 입력하지 않았습니다.',
+                  pattern: {
+                    value: regUri,
+                    message: '리다이렉트 URI를 형식에 맞게 입력해주세요',
+                  },
+                  maxLength: 254,
+                })}
+              />
+              <Input
+                label={'서비스 URI'}
+                errors={!!errors.error}
+                register={register('serviceUri', {
+                  required: '사이트 URI를 입력하지 않았습니다.',
+                  pattern: {
+                    value: regUri,
+                    message: '사이트 URI를 형식에 맞게 입력해주세요',
+                  },
+                  maxLength: 254,
+                })}
+              />
+            </div>
+            <S.Scope>
+              <p>공개 여부 :</p>
+              <div onClick={ChangeSeviceScope}>
+                {serviceScope === 'PUBLIC' ? (
+                  <SVG.ServicePublic />
+                ) : (
+                  <SVG.ServicePrivate />
+                )}
+              </div>
+            </S.Scope>
+          </S.ContentSection>
+          <button type="submit">수정</button>
+        </form>
+      </S.Section>
+      <S.Section>
+        <S.ImgContainer>
+          <label htmlFor="file">
+            <SVG.AddServiceImg />
+            <div>이미지 업로드</div>
+          </label>
+          <input type="file" name="image" id="file" />
+        </S.ImgContainer>
+        <S.CopyWrapper>
+          <S.CopyTitle>
+            <p>
+              클라이언트 ID
+              <i
+                onClick={() => {
+                  navigator.clipboard.writeText(watch('clientId'));
+                  toast.info('텍스트가 복사 되었습니다.');
+                }}
+              >
+                <SVG.CopyIcon />
+              </i>
+            </p>
+            <h4>{watch('clientId')}</h4>
+          </S.CopyTitle>
+          <S.CopyTitle>
+            <p>
+              클라이언트 SECRET
+              <i
+                onClick={() => {
+                  navigator.clipboard.writeText(watch('clientSecret'));
+                  toast.info('텍스트가 복사 되었습니다.');
+                }}
+              >
+                <SVG.CopyIcon />
+              </i>
+            </p>
+            <h4>{watch('clientSecret')}</h4>
+          </S.CopyTitle>
+        </S.CopyWrapper>
+      </S.Section>
     </S.Wrapper>
   );
 }

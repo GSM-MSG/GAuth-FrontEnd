99481b34d14006f138a2c103530321bb2bb4bb21
송현우 <shw9052@naver.com>
2023-11-08 23:37:59 +0900

    🔥Delete other text

--- src/components/common/Auth/NewPasswordCommon.tsx
+++ src/components/common/Auth/NewPasswordCommon.tsx
@@ -103,7 +103,7 @@ export default function NewPasswordCommon({
                 },
               },
             })}
-            maxLength={72}비
+            maxLength={72}
           />
           {error && <p>{error}</p>}
         </InputWrapper>

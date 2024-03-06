2397f0dff543f0aaaa8118fab5c4cbca9e7187a7
SeoJumee <official.jumee@gmail.com>
2024-03-06 16:43:06 +0900

    :lipstick: Add service owner list

---
+++ src/components/ServiceOwnerList/index.tsx
@@ -0,0 +1,59 @@
+import { useSetRecoilState } from 'recoil';
+import XIcon from '../../../public/svg/XIcon';
+import { ServiceOwnerModal } from '../../Atom/Atoms';
+import Portal from '../common/Portal';
+import SearchBar from '../common/SearchBar';
+import * as S from './style';
+
+interface Props {
+  onClose: () => void;
+}
+
+export default function ServiceOwnerList({ onClose }: Props) {
+  const setServiceOwnerModal = useSetRecoilState(ServiceOwnerModal);
+
+  return (
+    <Portal onClose={onClose}>
+      <S.Container>
+        <S.TitleWrapper>
+          <h1>소유자 이전</h1>
+          <div onClick={onClose}>
+            <XIcon />
+          </div>
+        </S.TitleWrapper>
+        <SearchBar placeholder="이름으로 검색하기..." />
+        <S.TableWrapper>
+          <S.Table>
+            <thead>
+              <tr>
+                <th>이름</th>
+                <th>학년</th>
+                <th>반</th>
+                <th>번호</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.from({ length: 20 }).map((idx: any) => (
+                <tr key={idx}>
+                  <td>서주미</td>
+                  <td>3</td>
+                  <td>2</td>
+                  <td>6</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        setServiceOwnerModal('delete');
+                      }}
+                    >
+                      권한 양도
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </S.Table>
+        </S.TableWrapper>
+      </S.Container>
+    </Portal>
+  );
+}
---
+++ src/components/ServiceOwnerList/style.ts
@@ -0,0 +1,111 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 710px;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+  background-color: #fafafa;
+  border-radius: 10px 10px 0 0;
+  padding: 28px;
+  padding-bottom: 0;
+
+  @media (max-width: 755px) {
+    width: 94vw;
+  }
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  h1 {
+    font-size: 18px;
+  }
+
+  svg {
+    cursor: pointer;
+  }
+`;
+
+export const TableWrapper = styled.div`
+  width: 100%;
+  background: #ffffff;
+  border: 1px solid #e0e0e0;
+  border-radius: 10px;
+  overflow: auto;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+
+  th,
+  td {
+    text-align: left;
+    width: 10%;
+    :nth-of-type(1) {
+      width: 15%;
+      padding-left: 20px;
+    }
+
+    :nth-of-type(4) {
+      width: 30%;
+
+      @media (max-width: 700px) {
+        width: 20%;
+      }
+
+      @media (max-width: 590px) {
+        width: 15%;
+      }
+
+      @media (max-width: 550px) {
+        width: 10%;
+      }
+    }
+  }
+
+  tr {
+    font-size: 14px;
+    line-height: 17px;
+    color: #1c1c1c;
+
+    th {
+      font-weight: 500;
+      font-size: 13px;
+      color: #929292;
+      padding: 20px 0;
+    }
+
+    td {
+      padding: 24px 0px;
+
+      button {
+        color: #3f99ee;
+        padding: 8px 12px;
+        border: 1px solid #9ccbf6;
+        border-radius: 10px;
+        background: none;
+        cursor: pointer;
+
+        @media (max-width: 510px) {
+          font-size: 12px;
+        }
+
+        @media (max-width: 480px) {
+          word-break: keep-all;
+          padding: 8px;
+        }
+      }
+    }
+  }
+
+  tbody {
+    tr {
+      border-top: 1px solid #e4e4e4;
+    }
+  }
+`;

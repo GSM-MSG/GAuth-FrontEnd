81132b3e24ff709363ee14538dceb827790c7bb5
happytaeyoon <s23021@gsm.hs.kr>
2024-06-03 17:26:06 +0900

    ✨ Add add-collaborator

---
+++ src/components/NewService/ServiceOwnerList/index.tsx
@@ -0,0 +1,118 @@
+import { useEffect, useState } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import XIcon from '../../../../public/svg/XIcon';
+import {
+  Search,
+  ServiceOwnerUserId,
+  ServiceOwnerModal,
+  StuList,
+  selectedUsersState,
+} from '../../../Atom/Atoms';
+import { useUserList } from '../../../hooks/useUserList';
+import Portal from '../../common/Portal';
+import SearchBar from '../../common/SearchBar';
+import * as S from './style';
+import { AddBtn } from '../../../../public/svg/AddBtn';
+import { AddcompleteBtn } from '../../../../public/svg/AddcompleteBtn';
+
+interface Props {
+  onClose: () => void;
+  userId: number;
+  isAdd: boolean;
+}
+
+export default function ServiceOwnerList({ onClose, userId, isAdd }: Props) {
+  const setServiceOwnerModal = useSetRecoilState(ServiceOwnerModal);
+  const search = useRecoilValue(Search);
+  const stuList = useRecoilValue(StuList);
+  const setServiceOwnerUserId = useSetRecoilState(ServiceOwnerUserId);
+  const setSelectedUsers = useSetRecoilState(selectedUsersState);
+  const selectedUsers = useRecoilValue(selectedUsersState);
+
+  const { getUserList } = useUserList({
+    defaultUri: `/user/user-list?grade=0&classNum=0&keyword=&role=ROLE_STUDENT`,
+  });
+
+  useEffect(() => {
+    getUserList();
+  }, []);
+
+  const handleAddUser = (id: number, name: string, profileUrl: string) => {
+    if (!isUserSelected(id)) {
+      setSelectedUsers((prev) => [...prev, { id, name, profileUrl }]);
+    } else {
+      setSelectedUsers((prev) => prev.filter((user) => user.id !== id));
+    }
+  };
+
+  const isUserSelected = (id: number) => {
+    return selectedUsers.find((user) => user.id === id);
+  };
+
+  const filteredStuList = isAdd
+    ? stuList.filter((e) => e.name?.includes(search) && e.id !== userId)
+    : stuList.filter((e) => isUserSelected(e.id));
+
+  return (
+    <Portal onClose={onClose}>
+      <S.Container>
+        <S.TitleWrapper>
+          {isAdd ? <h1>공동작업자 추가</h1> : <h1>공동작업자 목록</h1>}
+          <div onClick={onClose}>
+            <XIcon />
+          </div>
+        </S.TitleWrapper>
+        <SearchBar placeholder="이름으로 검색하기..." />
+        <S.TableWrapper>
+          <S.Table>
+            <thead>
+              <tr>
+                <th>이름</th>
+                <th>학년</th>
+                <th>반</th>
+                <th>번호</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredStuList.map((e, idx) => (
+                <tr key={idx}>
+                  <td>{e.name}</td>
+                  <td>{e.grade}</td>
+                  <td>{e.classNum}</td>
+                  <td>{e.num}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        setServiceOwnerModal('assignment');
+                        setServiceOwnerUserId(e.id);
+                        if (e.name) {
+                          handleAddUser(e.id, e.name, e.profileUrl);
+                        }
+                      }}
+                    >
+                      {isAdd ? (
+                        isUserSelected(e.id) ? (
+                          <>
+                            <AddcompleteBtn />
+                            추가완료
+                          </>
+                        ) : (
+                          <>
+                            <AddBtn />
+                            추가하기
+                          </>
+                        )
+                      ) : (
+                        <p>공동작업자</p>
+                      )}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </S.Table>
+        </S.TableWrapper>
+      </S.Container>
+    </Portal>
+  );
+}
--- src/components/NewService/index.tsx
+++ src/components/NewService/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as SVG from '../../../public/svg';
 import useFetch from '../../hooks/useFetch';
@@ -7,12 +7,19 @@ import { NewServiceForm, ResNewService } from '../../types/ResAddService';
 import Input from '../common/Input';
 import ServiceInfoModal from './InfoModal';
 import * as S from './style';
+import ServiceOwnerList from './ServiceOwnerList';
+import { useUser } from '../../hooks/useUser';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { selectedUsersState } from '../../Atom/Atoms';
 
 export default function NewServicePage() {
   const regUrl =
     /^(http(s)?:\/\/|www.)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}([\/a-z0-9-%#?&=\w])+(\.[a-z0-9]{2,4}(\?[\/a-z0-9-%#?&=\w]+)*)*/gi;
 
   const [modal, setModal] = useState<boolean>(false);
+  const [Ownermodal, setOwnermodal] = useState<boolean>(false);
+  const [isAdd, setIsAdd] = useState<boolean>(false);
+  const [serviceId, setServiceId] = useState<number>();
 
   const serviceDefaultData: ResNewService = {
     clientId: '',
@@ -23,7 +30,7 @@ export default function NewServicePage() {
     serviceScope: 'PUBLIC',
     serviceImgUrl: '',
   };
-
+  const [user] = useUser();
   const [serviceData, setServiceData] =
     useState<ResNewService>(serviceDefaultData);
 
@@ -59,8 +66,13 @@ export default function NewServicePage() {
       setServiceData(data);
       setModal(true);
       reset();
+      setServiceId(data.id);
     },
   });
+
+  useEffect(() => {
+    updateCoWorkersHandler();
+  }, [serviceId]);
 
   const { fetch: uploadImage } = useFetch<{ imageURL: string }>({
     url: '/image',
@@ -105,6 +117,25 @@ export default function NewServicePage() {
       }),
   });
 
+  const selectedUsers = useRecoilValue(selectedUsersState);
+  const setSelectedUsers = useSetRecoilState(selectedUsersState);
+
+  useEffect(() => {
+    setSelectedUsers([]);
+  }, [setSelectedUsers]);
+
+  const { fetch: patchUpdateCoWorkers } = useFetch({
+    url: `/client/${serviceId}/co-worker`,
+    method: 'PATCH',
+  });
+
+  const updateCoWorkersHandler = () => {
+    selectedUsers.forEach(async (user) => {
+      await patchUpdateCoWorkers({ userId: user.id });
+    });
+    setSelectedUsers([]);
+  };
+
   return (
     <S.Layout>
       <S.Wrapper>
@@ -113,102 +144,190 @@ export default function NewServicePage() {
             <h1>서비스를 등록해볼까요?</h1>
             <h3>서비스 등록도, 학생정보 수집도 쉽게!</h3>
           </S.TitleSection>
-          <S.InputContainer>
-            <Input
-              label="서비스명"
-              errors={!!errors.serviceName}
-              message={errors.serviceName?.message}
-              register={register('serviceName', {
-                required: '서비스명을 입력하지 않았습니다.',
-                pattern: {
-                  value: /\S+/,
-                  message: '서비스명을 입력하지 않았습니다.',
-                },
-              })}
-              maxLength={20}
-            />
-            <Input
-              label="리다이렉트 URL"
-              errors={!!errors.redirectUri}
-              message={errors.redirectUri?.message}
-              register={register('redirectUri', {
-                required: '리다이렉트 URI를 입력하지 않았습니다.',
-                pattern: {
-                  value: regUrl,
-                  message: '리다이렉트 URL를 형식에 맞게 입력해주세요',
-                },
-              })}
-              maxLength={254}
-            />
-            <Input
-              label="사이트 URL"
-              errors={!!errors.serviceUri}
-              message={errors.serviceUri?.message}
-              register={register('serviceUri', {
-                required: '서비스 URI를 입력하지 않았습니다.',
-                pattern: {
-                  value: regUrl,
-                  message: '서비스 URI를 형식에 맞게 입력해주세요',
-                },
-              })}
-              maxLength={254}
-            />
-            <S.ImgContainer>
-              <label htmlFor="file">
-                {serviceData.serviceImgUrl ? (
-                  <S.UploadContainer>
-                    <S.DeleteServiceWrapper
-                      onClick={(e) => {
-                        e.preventDefault();
-                        deleteImage();
-                      }}
-                    >
-                      <SVG.DeleteServiceImg />
-                    </S.DeleteServiceWrapper>
-                    <Image
-                      src={serviceData.serviceImgUrl}
-                      alt="업로드한 이미지"
-                      layout="fill"
-                      objectFit="cover"
-                    />
-                  </S.UploadContainer>
-                ) : (
-                  <>
-                    <SVG.AddServiceImg />
-                    <div>이미지 업로드</div>
-                  </>
-                )}
-              </label>
-              <input
-                type="file"
-                name="image"
-                id="file"
-                onChange={(e) => {
-                  const selectedFile = e.target.files?.[0];
-                  if (selectedFile) {
-                    handleFileUpload(selectedFile);
-                  }
-                }}
+          <S.Container>
+            <S.InputContainer>
+              <Input
+                label="서비스명"
+                errors={!!errors.serviceName}
+                message={errors.serviceName?.message}
+                register={register('serviceName', {
+                  required: '서비스명을 입력하지 않았습니다.',
+                  pattern: {
+                    value: /\S+/,
+                    message: '서비스명을 입력하지 않았습니다.',
+                  },
+                })}
+                maxLength={20}
               />
-              <p>이미지 용량 제한은 5MB입니다.</p>
-            </S.ImgContainer>
-            <span>
-              공개여부:{' '}
-              {serviceScope === 'PUBLIC' ? (
-                <div onClick={handleClose}>
-                  <SVG.ServicePublic />
-                </div>
-              ) : (
-                <div onClick={handleClose}>
-                  <SVG.ServicePrivate />
-                </div>
+              <Input
+                label="리다이렉트 URL"
+                errors={!!errors.redirectUri}
+                message={errors.redirectUri?.message}
+                register={register('redirectUri', {
+                  required: '리다이렉트 URI를 입력하지 않았습니다.',
+                  pattern: {
+                    value: regUrl,
+                    message: '리다이렉트 URL를 형식에 맞게 입력해주세요',
+                  },
+                })}
+                maxLength={254}
+              />
+              <Input
+                label="사이트 URL"
+                errors={!!errors.serviceUri}
+                message={errors.serviceUri?.message}
+                register={register('serviceUri', {
+                  required: '서비스 URI를 입력하지 않았습니다.',
+                  pattern: {
+                    value: regUrl,
+                    message: '서비스 URI를 형식에 맞게 입력해주세요',
+                  },
+                })}
+                maxLength={254}
+              />
+              <S.ImgContainer>
+                <label htmlFor="file">
+                  {serviceData.serviceImgUrl ? (
+                    <S.UploadContainer>
+                      <S.DeleteServiceWrapper
+                        onClick={(e) => {
+                          e.preventDefault();
+                          deleteImage();
+                        }}
+                      >
+                        <SVG.DeleteServiceImg />
+                      </S.DeleteServiceWrapper>
+                      <Image
+                        src={serviceData.serviceImgUrl}
+                        alt="업로드한 이미지"
+                        layout="fill"
+                        objectFit="cover"
+                      />
+                    </S.UploadContainer>
+                  ) : (
+                    <>
+                      <SVG.AddServiceImg />
+                      <div>이미지 업로드</div>
+                    </>
+                  )}
+                </label>
+                <input
+                  type="file"
+                  name="image"
+                  id="file"
+                  onChange={(e) => {
+                    const selectedFile = e.target.files?.[0];
+                    if (selectedFile) {
+                      handleFileUpload(selectedFile);
+                    }
+                  }}
+                />
+                <p>이미지 용량 제한은 5MB입니다.</p>
+              </S.ImgContainer>
+              <span>
+                공개여부:{' '}
+                {serviceScope === 'PUBLIC' ? (
+                  <div onClick={handleClose}>
+                    <SVG.ServicePublic />
+                  </div>
+                ) : (
+                  <div onClick={handleClose}>
+                    <SVG.ServicePrivate />
+                  </div>
+                )}
+              </span>
+              <S.OwnerButton
+                type="button"
+                onClick={() => {
+                  setOwnermodal(true);
+                  setIsAdd(true);
+                }}
+              >
+                공동작업자 추가
+              </S.OwnerButton>
+              <S.Submit type="submit">등록</S.Submit>
+            </S.InputContainer>
+            <S.CollaboratorWrapper>
+              <S.ManagerWrapper
+                onClick={(e) => {
+                  setOwnermodal(true);
+                  setIsAdd(true);
+                }}
+              >
+                <p>공동작업자 추가</p>
+                <S.Add>
+                  <SVG.AddMangerBlue />
+                  <p>추가</p>
+                </S.Add>
+              </S.ManagerWrapper>
+              <S.MemberContainer>
+                {
+                  <S.Member>
+                    <S.Profile>
+                      {user.profileUrl ? (
+                        <S.Circle>
+                          <Image
+                            src={user.profileUrl}
+                            alt="프로필 이미지"
+                            width={24}
+                            height={24}
+                            objectFit="cover"
+                          />
+                        </S.Circle>
+                      ) : (
+                        <SVG.ProfileSmallFace />
+                      )}
+                      {user.name}
+                    </S.Profile>
+                    소유자
+                  </S.Member>
+                }
+                {selectedUsers.slice(0, 13).map((member, index) => (
+                  <S.Member key={index}>
+                    <S.Profile>
+                      {member.profileUrl ? (
+                        <S.Circle>
+                          <Image
+                            src={member.profileUrl}
+                            alt="프로필 이미지"
+                            width={24}
+                            height={24}
+                            objectFit="cover"
+                          />
+                        </S.Circle>
+                      ) : (
+                        <SVG.ProfileSmallFace />
+                      )}
+                      {member.name}
+                    </S.Profile>
+                    공동작업자
+                  </S.Member>
+                ))}
+              </S.MemberContainer>
+              {selectedUsers.length > 13 && (
+                <S.SeeMoreCollaborators
+                  onClick={() => {
+                    setOwnermodal(true);
+                    setIsAdd(false);
+                  }}
+                >
+                  <p>{selectedUsers.length - 13}명의 공동작업자 더보기</p>
+                  <SVG.ChevonRight />
+                </S.SeeMoreCollaborators>
               )}
-            </span>
-          </S.InputContainer>
-          <S.Submit type="submit">등록</S.Submit>
+            </S.CollaboratorWrapper>
+          </S.Container>
         </S.Form>
+
         {modal && (
           <ServiceInfoModal serviceData={serviceData} onClose={onClose} />
+        )}
+        {Ownermodal && (
+          <ServiceOwnerList
+            userId={user.userId}
+            onClose={() => setOwnermodal(false)}
+            isAdd={isAdd}
+          />
         )}
       </S.Wrapper>
     </S.Layout>
